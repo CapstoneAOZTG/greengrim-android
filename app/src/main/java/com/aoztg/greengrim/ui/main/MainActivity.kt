fe3c36d55b3486ae394fffe29f190d408766bb4d
박진성 <plashdof@gmail.com>
2023-09-24 08:10:28 +0900

    feat : bottomnavigation 로직 구현

--- app/src/main/java/com/aoztg/greengrim/ui/main/MainActivity.kt
+++ app/src/main/java/com/aoztg/greengrim/ui/main/MainActivity.kt
@@ -2,11 +2,32 @@ package com.aoztg.greengrim.ui.main
 
 
 import android.os.Bundle
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.ActivityMainBinding
 import com.aoztg.greengrim.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bnv.apply{
+            setupWithNavController(navController)
+        }
+
+        binding.bnv.itemIconTintList = null
+
     }
 }
