05b3527824dcf02dbb1e3991c860a4f62650b768
박진성 <plashdof@gmail.com>
2023-10-10 02:35:31 +0900

    etc : 테스트용 코드 추가

--- app/src/main/java/com/aoztg/greengrim/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aoztg/greengrim/ui/login/LoginActivity.kt
@@ -1,9 +1,25 @@
 package com.aoztg.greengrim.ui.login
 
+import android.content.Intent
+import android.os.Bundle
 import com.aoztg.greengrim.databinding.ActivityLoginBinding
 import com.aoztg.greengrim.ui.base.BaseActivity
+import com.aoztg.greengrim.ui.main.MainActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setListener()
+    }
+
+    private fun setListener(){
+
+        // TODO 홈화면 구현용 테스트코드
+        binding.btnKakaoLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
