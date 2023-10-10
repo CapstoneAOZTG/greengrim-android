b5a9186bb835c04116ee9262baa5bc71aa1d9496
박진성 <plashdof@gmail.com>
2023-10-10 21:09:21 +0900

    feat : signup, terms activity 셋팅

---
+++ app/src/main/java/com/aoztg/greengrim/ui/login/SignupActivity.kt
@@ -0,0 +1,7 @@
+package com.aoztg.greengrim.ui.login
+
+import com.aoztg.greengrim.databinding.ActivitySignupBinding
+import com.aoztg.greengrim.ui.base.BaseActivity
+
+class SignupActivity : BaseActivity<ActivitySignupBinding>(ActivitySignupBinding::inflate) {
+}
