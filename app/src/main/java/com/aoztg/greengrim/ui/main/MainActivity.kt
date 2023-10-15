45475522f7234e4eda5ceb5440e808e7eaeb02af
박진성 <plashdof@gmail.com>
2023-10-15 21:25:01 +0900

    #10 bottomnavigationview 커스텀 완료

--- app/src/main/java/com/aoztg/greengrim/ui/main/MainActivity.kt
+++ app/src/main/java/com/aoztg/greengrim/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.aoztg.greengrim.ui.main
 
 
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.forEach
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -12,6 +14,7 @@ import com.aoztg.greengrim.databinding.ActivityMainBinding
 import com.aoztg.greengrim.ui.base.BaseActivity
 import com.aoztg.greengrim.ui.home.HomeFragment
 import com.aoztg.greengrim.ui.home.HomeFragmentDirections
+import com.aoztg.greengrim.util.Constants.TAG
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
@@ -23,16 +26,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     }
 
     private fun setBottomNavigation(){
-        binding.bnv.itemIconTintList = null
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.navController
 
-        binding.bnv.apply{
-            setupWithNavController(navController)
-        }
-        binding.btnHome.setOnClickListener {
-            val action = HomeFragmentDirections.actionGlobalToHomeFragment()
-            navController.navigate(action)
+        with(binding){
+            bnv.itemIconTintList = null
+            bnv.selectedItemId = R.id.placeholder
+
+            bnv.apply{
+                setupWithNavController(navController)
+            }
+
+            btnHome.setOnClickListener {
+                val action = HomeFragmentDirections.actionGlobalToHomeFragment()
+                navController.navigate(action)
+                val menu = this.bnv.menu
+                menu.getItem(2).isChecked = true
+            }
+
         }
     }
 }
