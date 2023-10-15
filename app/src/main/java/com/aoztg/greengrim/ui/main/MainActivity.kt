4cd49835c222e0633e63ab39fc2d1977af7a6bb8
박진성 <plashdof@gmail.com>
2023-10-15 20:38:38 +0900

    #10 feat : navigation safeargs 적용, global home action 구현

--- app/src/main/java/com/aoztg/greengrim/ui/main/MainActivity.kt
+++ app/src/main/java/com/aoztg/greengrim/ui/main/MainActivity.kt
@@ -4,11 +4,14 @@ package com.aoztg.greengrim.ui.main
 import android.os.Bundle
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.ActivityMainBinding
 import com.aoztg.greengrim.ui.base.BaseActivity
+import com.aoztg.greengrim.ui.home.HomeFragment
+import com.aoztg.greengrim.ui.home.HomeFragmentDirections
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
@@ -26,6 +29,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
         binding.bnv.apply{
             setupWithNavController(navController)
+        }
+        binding.btnHome.setOnClickListener {
+            val action = HomeFragmentDirections.actionGlobalToHomeFragment()
+            navController.navigate(action)
         }
     }
 }
