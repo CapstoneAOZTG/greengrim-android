5db8b1164bc103e71756647beaba7fa070f59ffb
박진성 <plashdof@gmail.com>
2023-10-16 22:32:43 +0900

    #13 feat : home viewpager adapter 템플릿 구현

---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/home/adapter/HotNftHolder.kt
@@ -0,0 +1,14 @@
+package com.aoztg.greengrim.presentation.ui.home.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.aoztg.greengrim.databinding.ItemHomeHotChallengeBinding
+
+class HotNftHolder(private val binding : ItemHomeHotChallengeBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(
+        item : String,
+        listener : HomeItemClickListener?
+    ){
+
+    }
+}
