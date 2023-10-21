27ea297a1130d5fc11eb16d0151d4708bc18fba7
박진성 <plashdof@gmail.com>
2023-10-21 23:09:04 +0900

    #11 feat : Intro API 통신 구현

--- app/src/main/java/com/aoztg/greengrim/data/repository/IntroRepository.kt
+++ app/src/main/java/com/aoztg/greengrim/data/repository/IntroRepository.kt
@@ -1,10 +1,18 @@
 package com.aoztg.greengrim.data.repository
 
-import com.aoztg.greengrim.data.model.SignupPostData
+import com.aoztg.greengrim.data.model.LoginRequest
+import com.aoztg.greengrim.data.model.LoginResponse
+import com.aoztg.greengrim.data.model.SignupRequest
+import com.aoztg.greengrim.data.model.SignupResponse
+import retrofit2.Response
 
 interface IntroRepository {
 
-    suspend fun postSignup(
-        data : SignupPostData
-    ) : Boolean
+    suspend fun signup(
+        data: SignupRequest
+    ): Response<SignupResponse>
+
+    suspend fun login(
+        data: LoginRequest
+    ): Response<LoginResponse>
 }
--- app/src/main/java/com/aoztg/greengrim/data/repository/IntroRepositoryImpl.kt
+++ app/src/main/java/com/aoztg/greengrim/data/repository/IntroRepositoryImpl.kt
@@ -1,14 +1,22 @@
 package com.aoztg.greengrim.data.repository
 
+import com.aoztg.greengrim.data.model.LoginRequest
+import com.aoztg.greengrim.data.model.LoginResponse
+import com.aoztg.greengrim.data.model.SignupRequest
+import com.aoztg.greengrim.data.model.SignupResponse
 import com.aoztg.greengrim.data.remote.IntroAPI
-import com.aoztg.greengrim.data.model.SignupPostData
+import retrofit2.Response
 import javax.inject.Inject
 
-class IntroRepositoryImpl @Inject constructor(private val api : IntroAPI) : IntroRepository {
+class IntroRepositoryImpl @Inject constructor(private val api: IntroAPI) : IntroRepository {
 
-    override suspend fun postSignup(data: SignupPostData): Boolean {
+    override suspend fun signup(data: SignupRequest): Response<SignupResponse> {
+        return api.signup(data)
 
-        return false
+    }
+
+    override suspend fun login(data: LoginRequest): Response<LoginResponse> {
+        return api.login(data)
     }
 
 }
