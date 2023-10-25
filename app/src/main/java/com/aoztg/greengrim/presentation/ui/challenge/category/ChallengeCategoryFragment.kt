f38dfa18f5ba018d2ca2ca6b15da1913ef4cba05
박진성 <plashdof@gmail.com>
2023-10-26 02:51:43 +0900

    #18 feat : 챌린지 fragment 네이밍 변경

---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/category/ChallengeCategoryFragment.kt
@@ -0,0 +1,11 @@
+package com.aoztg.greengrim.presentation.ui.challenge.category
+
+
+import com.aoztg.greengrim.R
+import com.aoztg.greengrim.databinding.FragmentChallengeCategoryBinding
+import com.aoztg.greengrim.presentation.base.BaseFragment
+
+class ChallengeCategoryFragment :
+    BaseFragment<FragmentChallengeCategoryBinding>(R.layout.fragment_challenge_category) {
+
+}
