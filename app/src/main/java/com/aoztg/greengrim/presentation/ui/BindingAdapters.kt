c82ad27cc380717a56181d54297c1c39aedd6bad
박진성 <plashdof@gmail.com>
2023-10-30 08:54:03 +0900

    #18 fix : Home Chip 대신 TextView ChipGroup 에 삽입

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/BindingAdapters.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/BindingAdapters.kt
@@ -1,11 +1,13 @@
 package com.aoztg.greengrim.presentation.ui
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.aoztg.greengrim.R
 import com.bumptech.glide.Glide
+import com.google.android.material.chip.ChipGroup
 
 @BindingAdapter("imgResource")
 fun bindResource(imageView: ImageView, resource: Int) {
@@ -18,6 +20,28 @@ fun bindImg(imageView: ImageView, url: String) {
         .load(url)
         .error(R.drawable.test)
         .into(imageView)
+}
+
+@BindingAdapter("chipList")
+fun bindChips(chipGroup: ChipGroup, chips: List<String>) {
+
+    val colors = listOf(
+        R.drawable.shape_greenfill_nostroke_radius20,
+        R.drawable.shape_yellowfill_nostroke_radius20,
+        R.drawable.shape_purplefill_nostroke_radius20,
+        R.drawable.shape_grey2fill_nostroke_radius20
+    )
+
+    chips.forEach { data ->
+        val chip = TextView(chipGroup.context).apply {
+            text = data
+            setBackgroundResource(colors.random())
+            setTextAppearance(R.style.TextGgSmallBlack)
+            setPadding(20, 4, 20, 4)
+        }
+
+        chipGroup.addView(chip)
+    }
 }
 
 @BindingAdapter("list")
