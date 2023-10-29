c8cf7b4e55cb225eb1c6b59c2f763701f23f7a40
박진성 <plashdof@gmail.com>
2023-10-30 04:09:15 +0900

    #18 feat : recyclerview, img bindingAdapter 생성

---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/BindingAdapters.kt
@@ -0,0 +1,27 @@
+package com.aoztg.greengrim.presentation.ui
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.aoztg.greengrim.R
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imgResource")
+fun bindResource(imageView: ImageView, resource: Int) {
+    imageView.setImageResource(resource)
+}
+
+@BindingAdapter("imgUrl")
+fun bindImg(imageView: ImageView, url: String) {
+    Glide.with(imageView.context)
+        .load(url)
+        .error(R.drawable.ic_launcher_foreground)
+        .into(imageView)
+}
+
+@BindingAdapter("list")
+fun <T, VH : RecyclerView.ViewHolder> bindList(recyclerView: RecyclerView, list: List<T>) {
+    val adapter = recyclerView.adapter as ListAdapter<T, VH>
+    adapter.submitList(list)
+}
