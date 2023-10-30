cd574d2fc74b85ad32df4d9e644d8b204511d167
박진성 <plashdof@gmail.com>
2023-10-31 07:57:12 +0900

    #19 feat : Create Challenge Detail Setting

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/create/CreateChallengeDetailFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/create/CreateChallengeDetailFragment.kt
@@ -2,11 +2,14 @@ package com.aoztg.greengrim.presentation.ui.challenge.create
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentCreateChallengeDetailBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
 
 class CreateChallengeDetailFragment : BaseFragment<FragmentCreateChallengeDetailBinding>(R.layout.fragment_create_challenge_detail) {
+
+    private val viewModel : CreateChallengeDetailViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/create/CreateChallengeDetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.aoztg.greengrim.presentation.ui.challenge.create
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateChallengeDetailViewModel @Inject constructor() : ViewModel() {
+
+}
