61e4b1f1c7c0872fb4ee11f526e188160592044a
박진성 <plashdof@gmail.com>
2023-10-30 10:11:42 +0900

    #18 feat : 챌린지 디테일 로직 구현 (dummy data test)

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/detail/ChallengeDetailFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/detail/ChallengeDetailFragment.kt
@@ -1,9 +1,24 @@
 package com.aoztg.greengrim.presentation.ui.challenge.detail
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentChallengeDetailBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
 class ChallengeDetailFragment : BaseFragment<FragmentChallengeDetailBinding>(R.layout.fragment_challenge_detail) {
 
+    private val viewModel: ChallengeDetailViewModel by viewModels()
+    private val args: ChallengeDetailFragmentArgs by navArgs()
+    private val id by lazy{args.id}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
+        viewModel.getChallengeDetailInfo()
+    }
 }
---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/detail/ChallengeDetailViewModel.kt
@@ -0,0 +1,57 @@
+package com.aoztg.greengrim.presentation.ui.challenge.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aoztg.greengrim.presentation.ui.LoadingState
+import com.aoztg.greengrim.presentation.ui.challenge.model.ChallengeDetail
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+data class ChallengeDetailUiState(
+    val loading: LoadingState = LoadingState.Empty
+)
+
+@HiltViewModel
+class ChallengeDetailViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ChallengeDetailUiState())
+    val uiState: StateFlow<ChallengeDetailUiState> = _uiState.asStateFlow()
+
+    private val _challengeDetail = MutableStateFlow(ChallengeDetail())
+    val challengeDetail: StateFlow<ChallengeDetail> = _challengeDetail.asStateFlow()
+
+    fun getChallengeDetailInfo() {
+        viewModelScope.launch {
+
+            _uiState.update {
+                it.copy(
+                    loading = LoadingState.IsLoading(true)
+                )
+            }
+
+            _challengeDetail.value = ChallengeDetail(
+                "test", "인하대학교 쓰레기 줍기",
+                "함께 쓰레기를 주우며 인증하고 키워드 얻어봐요! 각자 취미에 대해 편안하게 이야기 할 수 있는 챌린지 방입니다. 인하대 후문에서 쓰레기 줍고 있습니다!",
+                listOf("줍킹", "티켓 5/30"),
+                listOf("인증 15회", "최소 인증 수 2회", "키워드 #줍다", "인원 15/100"),
+                "2023 09-28 개설"
+            )
+
+
+            delay(1000)
+
+            _uiState.update {
+                it.copy(
+                    loading = LoadingState.IsLoading(false)
+                )
+            }
+        }
+    }
+
+}
