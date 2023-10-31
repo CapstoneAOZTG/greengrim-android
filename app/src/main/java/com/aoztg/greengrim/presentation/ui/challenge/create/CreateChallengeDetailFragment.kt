badb426dbed0d8159e18501042dcd007dda9a6f5
박진성 <plashdof@gmail.com>
2023-10-31 12:01:30 +0900

    #19 feat : seekbar, edittext 데이터 바인딩 적용. uiState 구현

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/create/CreateChallengeDetailFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/create/CreateChallengeDetailFragment.kt
@@ -1,15 +1,22 @@
 package com.aoztg.greengrim.presentation.ui.challenge.create
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentCreateChallengeDetailBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
+import com.aoztg.greengrim.presentation.ui.main.MainViewModel
 
-class CreateChallengeDetailFragment : BaseFragment<FragmentCreateChallengeDetailBinding>(R.layout.fragment_create_challenge_detail) {
+class CreateChallengeDetailFragment :
+    BaseFragment<FragmentCreateChallengeDetailBinding>(R.layout.fragment_create_challenge_detail) {
 
+    private val parentViewModel: MainViewModel by activityViewModels()
     private val viewModel: CreateChallengeDetailViewModel by viewModels()
     private val args: CreateChallengeDetailFragmentArgs by navArgs()
     private val category by lazy { args.category }
@@ -17,7 +24,26 @@ class CreateChallengeDetailFragment : BaseFragment<FragmentCreateChallengeDetail
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.pvm = parentViewModel
         binding.vm = viewModel
         binding.tvHeader.text = "$category 챌린지 생성"
+        imageObserver()
+    }
+
+    private fun imageObserver() {
+        repeatOnStarted {
+            parentViewModel.image.collect {
+                viewModel.setImageUrl(it)
+            }
+        }
+    }
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("progressText")
+fun bindProgressText(view: TextView, progressState: ProgressState) {
+    when (progressState) {
+        is ProgressState.Changed -> view.text = progressState.text
+        else -> {}
     }
 }
--- app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/create/CreateChallengeDetailViewModel.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/challenge/create/CreateChallengeDetailViewModel.kt
@@ -1,10 +1,79 @@
 package com.aoztg.greengrim.presentation.ui.challenge.create
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
+
+
+data class CreateChallengeDetailUiState(
+    val certificateProgressState: ProgressState = ProgressState.Empty,
+    val ticketProgressState: ProgressState = ProgressState.Empty,
+    val minCertificateProgressState: ProgressState = ProgressState.Empty
+)
+
+sealed class ProgressState {
+    object Empty : ProgressState()
+    data class Changed(val text: String) : ProgressState()
+}
 
 @HiltViewModel
 class CreateChallengeDetailViewModel @Inject constructor() : ViewModel() {
 
+    private val _uiState = MutableStateFlow(CreateChallengeDetailUiState())
+    val uiState: StateFlow<CreateChallengeDetailUiState> = _uiState.asStateFlow()
+
+    val title = MutableStateFlow("")
+    val description = MutableStateFlow("")
+    private var imageUrl = ""
+
+    val certificateProgress = MutableStateFlow(0)
+    val ticketProgress = MutableStateFlow(0)
+    val minCertificateProgress = MutableStateFlow(0)
+
+    init {
+        observeSeekBar()
+    }
+
+    private fun observeSeekBar() {
+        certificateProgress.onEach { progress ->
+            _uiState.update { state ->
+                state.copy(
+                    certificateProgressState = ProgressState.Changed(
+                        (10 + (progress * 5)).toString() + "회"
+                    )
+                )
+            }
+        }.launchIn(viewModelScope)
+
+        ticketProgress.onEach { progress ->
+            _uiState.update { state ->
+                state.copy(
+                    ticketProgressState = ProgressState.Changed(
+                        (20 + (progress * 20)).toString() + "개"
+                    )
+                )
+            }
+        }.launchIn(viewModelScope)
+
+        minCertificateProgress.onEach { progress ->
+            _uiState.update { state ->
+                state.copy(
+                    minCertificateProgressState = ProgressState.Changed(
+                        "주 " + (2 + progress).toString() + "회"
+                    )
+                )
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    fun setImageUrl(url: String) {
+        imageUrl = url
+    }
 }
