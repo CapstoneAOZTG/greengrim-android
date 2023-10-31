cae3ae7f13911a5b4de934b5c20e8fdcfa895706
박진성 <plashdof@gmail.com>
2023-10-31 12:00:02 +0900

    #19 feat : MainActivity, MainViewModel 갤러리 연동, image To Uri 로직 구현

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/main/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.aoztg.greengrim.presentation.ui.main
 
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -12,7 +21,8 @@ import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.ActivityMainBinding
 import com.aoztg.greengrim.presentation.base.BaseActivity
 import com.aoztg.greengrim.presentation.ui.home.HomeFragmentDirections
-import com.aoztg.greengrim.presentation.ui.main.event.MainEvent
+import com.aoztg.greengrim.presentation.util.Constants
+import com.aoztg.greengrim.presentation.util.toMultiPart
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +30,19 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     private lateinit var navController: NavController
     private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var neededPermissionList: MutableList<String>
+    private val requiredPermissionList =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arrayOf(  // 안드로이드 13 이상 필요한 권한들
+                Manifest.permission.READ_MEDIA_IMAGES
+            )
+        } else {
+            arrayOf(  // 안드로이드 13 미만 필요한 권한들
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,12 +89,74 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     private fun setEventObserver() {
         repeatOnStarted {
-            viewModel.eventFlow.collect {
+            viewModel.event.collect {
                 when (it) {
                     is MainEvent.HideBottomNav -> binding.layoutBnv.visibility = View.INVISIBLE
                     is MainEvent.ShowBottomNav -> binding.layoutBnv.visibility = View.VISIBLE
+                    is MainEvent.GoToGallery -> onCheckPermissions()
                 }
             }
         }
     }
+
+    private fun onCheckPermissions() {
+        neededPermissionList = mutableListOf()
+
+        requiredPermissionList.forEach { permission ->
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) neededPermissionList.add(permission)
+        }
+
+        if (neededPermissionList.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                neededPermissionList.toTypedArray(),
+                Constants.RC_PERMISSION
+            )
+        } else {
+            openGallery()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.RC_PERMISSION) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openGallery()
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryLauncher.launch(galleryIntent)
+    }
+
+
+    private val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+            if (result.resultCode == Activity.RESULT_OK) {
+                val uri = result.data?.data
+
+                uri?.let {
+                    viewModel.imageToUrl(it.toMultiPart(this))
+                }
+            }
+        }
 }
