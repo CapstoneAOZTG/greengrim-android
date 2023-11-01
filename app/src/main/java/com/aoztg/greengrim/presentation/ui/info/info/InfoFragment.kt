ff5706f2ab75bb76a2430337fc0b664ff659e607
박진성 <plashdof@gmail.com>
2023-11-02 08:08:01 +0900

    #31 feat : SettingBottomSheet 호출로직 구현

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/info/info/InfoFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/info/info/InfoFragment.kt
@@ -3,6 +3,7 @@ package com.aoztg.greengrim.presentation.ui.info.info
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentInfoBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
@@ -25,17 +26,56 @@ class InfoFragment : BaseFragment<FragmentInfoBinding>(R.layout.fragment_info) {
         repeatOnStarted {
             viewModel.events.collect {
                 when (it) {
-                    is InfoEvents.ShowBottomSheet -> {
-                    }
+                    is InfoEvents.ShowBottomSheet -> showBottomSheet()
                 }
             }
         }
     }
 
-//    private fun showBottomSheet() {
-//        getInfoSettingSheet(
-//            requireContext(),
-//            onEditProfileClickListener =
-//        )
-//    }
+    private fun showBottomSheet() {
+        getInfoSettingSheet(
+            requireContext(),
+            onEditProfileClickListener = ::navigateToEditProfile,
+            onNotificationClickListener = ::navigateToNotification,
+            onEditPasswordClickListener = ::navigateToEditPassword,
+            onAnnouncementClickListener = ::navigateToAnnouncement,
+            onTermsClickListener = ::navigateToTerms,
+            onAppVersionClickListener = ::navigateToAppVersion,
+            onLogoutClickListener = ::logout,
+            onWithdrawalClickListener = ::withdrawal,
+        ).show()
+    }
+
+    private fun navigateToEditProfile() {
+        val action = InfoFragmentDirections.actionInfoFragmentToEditProfileFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun navigateToNotification() {
+        showCustomToast("알림으로 이동")
+    }
+
+    private fun navigateToEditPassword() {
+        showCustomToast("패스워드 변경으로 이동")
+    }
+
+    private fun navigateToAnnouncement() {
+        showCustomToast("공지사항으로 이동")
+    }
+
+    private fun navigateToTerms() {
+        showCustomToast("이용 약관으로 이동")
+    }
+
+    private fun navigateToAppVersion() {
+        showCustomToast("앱 버전으로 이동")
+    }
+
+    private fun logout() {
+        showCustomToast("로그아웃")
+    }
+
+    private fun withdrawal() {
+        showCustomToast("회원탈퇴")
+    }
 }
--- app/src/main/java/com/aoztg/greengrim/presentation/ui/info/info/InfoViewModel.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/info/info/InfoViewModel.kt
@@ -9,19 +9,18 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-sealed class InfoEvents{
+sealed class InfoEvents {
     object ShowBottomSheet : InfoEvents()
 }
 
 @HiltViewModel
-class InfoViewModel @Inject constructor(): ViewModel() {
+class InfoViewModel @Inject constructor() : ViewModel() {
 
     private val _events = MutableSharedFlow<InfoEvents>()
     val events: SharedFlow<InfoEvents> = _events.asSharedFlow()
 
-
-    fun showBottomSheet(){
-        viewModelScope.launch{
+    fun showBottomSheet() {
+        viewModelScope.launch {
             _events.emit(InfoEvents.ShowBottomSheet)
         }
     }
