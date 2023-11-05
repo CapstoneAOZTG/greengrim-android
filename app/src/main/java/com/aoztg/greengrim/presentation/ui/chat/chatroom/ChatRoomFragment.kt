2355a2e7ee9f3badf5b686025c9b65d0cf584405
박진성 <plashdof@gmail.com>
2023-11-05 10:40:32 +0900

    #32 feat : Chat Room Event 정의, 분기처리 구현

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/chatroom/ChatRoomFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/chatroom/ChatRoomFragment.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentChatRoomBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
 import com.aoztg.greengrim.presentation.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class ChatRoomFragment : BaseFragment<FragmentChatRoomBinding>(R.layout.fragment_chat_room) {
@@ -22,6 +23,35 @@ class ChatRoomFragment : BaseFragment<FragmentChatRoomBinding>(R.layout.fragment
 
         binding.vm = viewModel
         parentViewModel.hideBNV()
+        initEventsObserver()
+    }
+
+    private fun initEventsObserver() {
+        repeatOnStarted {
+            viewModel.events.collect {
+                when (it) {
+                    is ChatRoomEvents.ShowPopupMenu -> {}
+                    is ChatRoomEvents.NavigateBack -> findNavController().navigateUp()
+                    is ChatRoomEvents.NavigateToCertificationList -> findNavController().toCertificationList(
+                        it.id
+                    )
+
+                    is ChatRoomEvents.NavigateToMakeCertification -> findNavController().toMakeCertification(
+                        it.id
+                    )
+                }
+            }
+        }
+    }
+
+    private fun NavController.toCertificationList(id: String) {
+        val action = ChatRoomFragmentDirections.actionChatRoomFragmentToCertificationListFragment(id)
+        this.navigate(action)
+    }
+
+    private fun NavController.toMakeCertification(id: String) {
+        val action = ChatRoomFragmentDirections.actionChatRoomFragmentToMakeCertificationFragment(id)
+        this.navigate(action)
     }
 
 }
--- app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/chatroom/ChatRoomViewModel.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/chatroom/ChatRoomViewModel.kt
@@ -5,23 +5,37 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aoztg.greengrim.presentation.util.Constants.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class ChatRoomUiState(
     val editTextState: Boolean = false
 )
 
+sealed class ChatRoomEvents {
+    object NavigateBack : ChatRoomEvents()
+    object ShowPopupMenu : ChatRoomEvents()
+    data class NavigateToMakeCertification(val id: String) : ChatRoomEvents()
+    data class NavigateToCertificationList(val id: String) : ChatRoomEvents()
+}
+
 @HiltViewModel
 class ChatRoomViewModel @Inject constructor() : ViewModel() {
 
     private val _uiState = MutableStateFlow(ChatRoomUiState())
     val uiState: StateFlow<ChatRoomUiState> = _uiState.asStateFlow()
+
+    private val _events = MutableSharedFlow<ChatRoomEvents>()
+    val events: SharedFlow<ChatRoomEvents> = _events.asSharedFlow()
 
     val chatMessage = MutableStateFlow("")
 
@@ -31,7 +45,7 @@ class ChatRoomViewModel @Inject constructor() : ViewModel() {
 
     private fun observeChatMessage() {
         chatMessage.onEach {
-            Log.d(TAG,it)
+            Log.d(TAG, it)
             if (it.isNotBlank()) {
                 _uiState.update { state ->
                     state.copy(
@@ -46,5 +60,29 @@ class ChatRoomViewModel @Inject constructor() : ViewModel() {
                 }
             }
         }.launchIn(viewModelScope)
+    }
+
+    fun navigateBack() {
+        viewModelScope.launch {
+            _events.emit(ChatRoomEvents.NavigateBack)
+        }
+    }
+
+    fun navigateToMakeCertification(id: String) {
+        viewModelScope.launch {
+            _events.emit(ChatRoomEvents.NavigateToMakeCertification(id))
+        }
+    }
+
+    fun navigateToCertificationList(id: String) {
+        viewModelScope.launch {
+            _events.emit(ChatRoomEvents.NavigateToCertificationList(id))
+        }
+    }
+
+    fun showPopupMenu() {
+        viewModelScope.launch {
+            _events.emit(ChatRoomEvents.ShowPopupMenu)
+        }
     }
 }
