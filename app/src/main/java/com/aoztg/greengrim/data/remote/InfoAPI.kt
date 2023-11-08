0deca539440b88ce9fd6cc7fbbb385cc88f6c3da
박진성 <plashdof@gmail.com>
2023-11-08 12:45:41 +0900

    #30 feat : 내가 만든 챌린지 API 구현

--- app/src/main/java/com/aoztg/greengrim/data/remote/InfoAPI.kt
+++ app/src/main/java/com/aoztg/greengrim/data/remote/InfoAPI.kt
@@ -1,5 +1,6 @@
 package com.aoztg.greengrim.data.remote
 
+import com.aoztg.greengrim.data.model.ChallengeListResponse
 import com.aoztg.greengrim.data.model.GetProfileResponse
 import com.aoztg.greengrim.data.model.PatchProfileRequest
 import retrofit2.Response
@@ -7,6 +8,7 @@ import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
+import retrofit2.http.Query
 
 interface InfoAPI {
 
@@ -20,4 +22,11 @@ interface InfoAPI {
 
     @DELETE("/visitor/delete")
     suspend fun withdrawal(): Response<Unit>
+
+    @GET("/visitor/challenges")
+    suspend fun getMyChallengeList(
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("sort") sort: String
+    ): Response<ChallengeListResponse>
 }
--- app/src/main/java/com/aoztg/greengrim/data/repository/ChallengeRepository.kt
+++ app/src/main/java/com/aoztg/greengrim/data/repository/ChallengeRepository.kt
@@ -1,5 +1,7 @@
 package com.aoztg.greengrim.data.repository
 
+import com.aoztg.greengrim.data.model.ChallengeDetailResponse
+import com.aoztg.greengrim.data.model.ChallengeListResponse
 import com.aoztg.greengrim.data.model.CreateChallengeRequest
 import retrofit2.Response
 
@@ -8,4 +10,15 @@ interface ChallengeRepository {
     suspend fun createChallenge(
         data: CreateChallengeRequest
     ): Response<Unit>
+
+    suspend fun getChallengeDetail(
+        id: Int
+    ): Response<ChallengeDetailResponse>
+
+    suspend fun getChallengeList(
+        category: String,
+        page: Int,
+        size: Int,
+        sort: String
+    ): Response<ChallengeListResponse>
 }
