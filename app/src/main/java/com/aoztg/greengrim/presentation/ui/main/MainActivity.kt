ae24ac486ad1c648afa9eb003f612c72320859a1
박진성 <plashdof@gmail.com>
2023-11-10 17:42:13 +0900

    #42 feat : 외부 터치시 키보드 내리기 감지 제거

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/main/MainActivity.kt
@@ -251,22 +251,4 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         }
     }
 
-    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
-        if (event?.action === MotionEvent.ACTION_DOWN) {
-            val v = currentFocus
-            if (v is EditText) {
-                val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm: InputMethodManager =
-                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
-                }
-            }
-        }
-        return super.dispatchTouchEvent(event)
-    }
-
-
 }
