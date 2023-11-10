5631030fbb35b4fb01a74fd5231c94ff49f65087
박진성 <plashdof@gmail.com>
2023-11-10 18:09:43 +0900

    #42 feat : 챌린지 인증 생성하기 완료버튼 활성화 조건 data binding

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/createcertification/CreateCertificationFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/createcertification/CreateCertificationFragment.kt
@@ -1,16 +1,42 @@
 package com.aoztg.greengrim.presentation.ui.chat.createcertification
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentCreateCertificationBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
+import com.aoztg.greengrim.presentation.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CreateCertificationFragment: BaseFragment<FragmentCreateCertificationBinding>(R.layout.fragment_create_certification) {
 
+    private val parentViewModel: MainViewModel by activityViewModels()
+    private val viewModel: CreateCertificationViewModel by viewModels()
+
     private val args: CreateCertificationFragmentArgs by navArgs()
     private val challengeId by lazy { args.challengeId }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.pvm = parentViewModel
+        binding.vm = viewModel
+        initImgObserver()
+    }
+
+    private fun initImgObserver(){
+        repeatOnStarted {
+            parentViewModel.image.collect {
+                if (it.isNotBlank()) {
+                    viewModel.setImgUrl(it)
+                }
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/createcertification/CreateCertificationViewModel.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/createcertification/CreateCertificationViewModel.kt
@@ -1,10 +1,41 @@
 package com.aoztg.greengrim.presentation.ui.chat.createcertification
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aoztg.greengrim.presentation.ui.BaseState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
+
+
+data class CreateCertificationUiState(
+    val nextBtnState: BaseState = BaseState.Empty
+)
 
 @HiltViewModel
 class CreateCertificationViewModel @Inject constructor(): ViewModel() {
+
+    private val _uiState = MutableStateFlow(CreateCertificationUiState())
+    val uiState: StateFlow<CreateCertificationUiState> = _uiState.asStateFlow()
+
+    val description = MutableStateFlow("")
+    private val certificationImgUrl = MutableStateFlow("")
+
+    val isDataReady = combine(description, certificationImgUrl){ description, imgUrl ->
+        description.isNotBlank() && imgUrl.isNotBlank()
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(),
+        false
+    )
+
+    fun setImgUrl(url: String){
+        certificationImgUrl.value = url
+    }
 
 }
