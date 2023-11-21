15e0bd7e0226d384af3a3592a6c0bafe625c7aca
박진성 <plashdof@gmail.com>
2023-11-21 22:36:28 +0900

    #42 feat : Chat Repository 에 ChatId 관련 로직 추가

--- app/src/main/java/com/aoztg/greengrim/data/repository/ChatRepository.kt
+++ app/src/main/java/com/aoztg/greengrim/data/repository/ChatRepository.kt
@@ -1,5 +1,6 @@
 package com.aoztg.greengrim.data.repository
 
+import com.aoztg.greengrim.data.local.ChatIdEntity
 import com.aoztg.greengrim.data.model.ChatRoomsResponse
 import com.aoztg.greengrim.data.model.EnterChatResponse
 import retrofit2.Response
@@ -11,4 +12,10 @@ interface ChatRepository {
     ): Response<EnterChatResponse>
 
     suspend fun getChatRooms(): Response<List<ChatRoomsResponse>>
+
+    suspend fun getAllChatId(): List<ChatIdEntity>
+
+    suspend fun addChatId(chatIdEntity: ChatIdEntity): Unit
+
+    suspend fun deleteChatId(chatId: Int): Unit
 }
--- app/src/main/java/com/aoztg/greengrim/data/repository/ChatRepositoryImpl.kt
+++ app/src/main/java/com/aoztg/greengrim/data/repository/ChatRepositoryImpl.kt
@@ -1,13 +1,21 @@
 package com.aoztg.greengrim.data.repository
 
+import com.aoztg.greengrim.data.local.ChatIdDao
+import com.aoztg.greengrim.data.local.ChatIdEntity
 import com.aoztg.greengrim.data.model.ChatRoomsResponse
 import com.aoztg.greengrim.data.model.EnterChatResponse
 import com.aoztg.greengrim.data.remote.ChatAPI
 import retrofit2.Response
 import javax.inject.Inject
 
-class ChatRepositoryImpl @Inject constructor(private val api: ChatAPI): ChatRepository {
+class ChatRepositoryImpl @Inject constructor(
+    private val api: ChatAPI,
+    private val chatIdDao: ChatIdDao
+): ChatRepository {
 
     override suspend fun enterChat(challengeId: Int): Response<EnterChatResponse> = api.enterChat(challengeId)
     override suspend fun getChatRooms(): Response<List<ChatRoomsResponse>> = api.getChatList()
+    override suspend fun addChatId(chatIdEntity: ChatIdEntity): Unit = chatIdDao.addChatId(chatIdEntity)
+    override suspend fun getAllChatId(): List<ChatIdEntity> = chatIdDao.getAllChatId()
+    override suspend fun deleteChatId(chatId: Int): Unit = chatIdDao.deleteChatId(chatId)
 }
