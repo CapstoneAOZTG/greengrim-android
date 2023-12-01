731f02576366c4ccddafac530df43a28b6f0cf84
박진성 <plashdof@gmail.com>
2023-12-02 05:38:13 +0900

    #50 feat : CustomSnackBar 구현

---
+++ app/src/main/java/com/aoztg/greengrim/presentation/customview/CustomSnackBar.kt
@@ -0,0 +1,46 @@
+package com.aoztg.greengrim.presentation.customview
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.aoztg.greengrim.R
+import com.aoztg.greengrim.databinding.DialogCustomSnackbarBinding
+import com.google.android.material.snackbar.Snackbar
+
+class CustomSnackBar(
+    view: View,
+    private val text: String
+) {
+
+    companion object{
+
+        fun make(view: View) = VerifySnackBar(view)
+    }
+
+    private val context = view.context
+    private val snackbar = Snackbar.make(view, "", 5000).apply {
+        anchorView = view
+    }
+    private val snackbarLayout = snackbar.view as Snackbar.SnackbarLayout
+
+    private val binding: DialogCustomSnackbarBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.dialog_custom_snackbar, null, false)
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        with(snackbarLayout) {
+            removeAllViews()
+            setPadding(40, 0, 40, 0)
+            setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
+            addView(binding.root, 0)
+            binding.tvText.text = text
+        }
+    }
+
+    fun show() {
+        snackbar.show()
+    }
+}
