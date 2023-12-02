95dfc73c7dbae51f1d563e1ab9857cddf748a1f1
박진성 <plashdof@gmail.com>
2023-12-02 11:11:39 +0900

    #48 feat : ChatList loading 처리 구현

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/chatlist/ChatListViewModel.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/chat/chatlist/ChatListViewModel.kt
@@ -8,6 +8,7 @@ import com.aoztg.greengrim.presentation.chatmanager.model.UiUnReadChatData
 import com.aoztg.greengrim.presentation.ui.chat.mapper.toUiChatListItem
 import com.aoztg.greengrim.presentation.ui.chat.model.UiChatListItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -26,7 +27,10 @@ data class ChatListUiState(
 sealed class ChatListEvents {
     data class NavigateToChatRoom(val chatId: Int, val challengeId: Int) : ChatListEvents()
     data class ShowToastMessage(val msg: String) : ChatListEvents()
-    object CallUnReadChatData: ChatListEvents()
+    object CallUnReadChatData : ChatListEvents()
+    data class ShowSnackMessage(val msg: String) : ChatListEvents()
+    object ShowLoading : ChatListEvents()
+    object DismissLoading : ChatListEvents()
 }
 
 
@@ -43,6 +47,8 @@ class ChatListViewModel @Inject constructor(
 
     fun getChatList() {
         viewModelScope.launch {
+            _events.emit(ChatListEvents.ShowLoading)
+
             chatRepository.getChatRooms().let {
                 when (it) {
                     is BaseState.Success -> {
@@ -53,29 +59,28 @@ class ChatListViewModel @Inject constructor(
                                 }
                             )
                         }
+                        _events.emit(ChatListEvents.CallUnReadChatData)
                     }
 
                     is BaseState.Error -> {
-                        _events.emit(ChatListEvents.ShowToastMessage(it.msg))
+                        _events.emit(ChatListEvents.ShowSnackMessage(it.msg))
+                        _events.emit(ChatListEvents.DismissLoading)
                     }
                 }
-
-                _events.emit(ChatListEvents.CallUnReadChatData)
             }
-
         }
     }
 
-    fun setUnReadChatData(list: List<UiUnReadChatData>){
+    fun setUnReadChatData(list: List<UiUnReadChatData>) {
 
         // todo 좀더 효율적인 알고리즘으로 refactoring..
 
         _uiState.update { state ->
             state.copy(
-                uiChatListItem = uiState.value.uiChatListItem.map{ listData ->
+                uiChatListItem = uiState.value.uiChatListItem.map { listData ->
                     var newData = listData
-                    list.forEach {  unReadData ->
-                        if(listData.chatId == unReadData.chatId){
+                    list.forEach { unReadData ->
+                        if (listData.chatId == unReadData.chatId) {
                             newData = listData.copy(
                                 recentChat = unReadData.recentChat,
                                 recentTime = unReadData.recentChatTime,
@@ -87,6 +92,11 @@ class ChatListViewModel @Inject constructor(
                     newData
                 }
             )
+        }
+
+        viewModelScope.launch {
+            delay(200)
+            _events.emit(ChatListEvents.DismissLoading)
         }
     }
 
