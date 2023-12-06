1e4da96a1b4f445c6725af7ea3ab87cf97d3f873
박진성 <plashdof@gmail.com>
2023-12-06 20:53:12 +0900

    #58 feat : ingame viewModel 생성

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/catchgame/CatchInGameFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/catchgame/CatchInGameFragment.kt
@@ -2,13 +2,17 @@ package com.aoztg.greengrim.presentation.ui.catchgame
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentCatchIngameFragmentBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
-import com.aoztg.greengrim.presentation.customview.CatchGameOverDialog
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CatchInGameFragment: BaseFragment<FragmentCatchIngameFragmentBinding>(R.layout.fragment_catch_ingame_fragment) {
+
+    private val viewModel: CatchInGameViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/catchgame/CatchInGameViewModel.kt
@@ -0,0 +1,9 @@
+package com.aoztg.greengrim.presentation.ui.catchgame
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CatchInGameViewModel @Inject constructor(): ViewModel() {
+}
