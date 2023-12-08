233c518325e48e35bcb74ae44568b596e75953f2
박진성 <plashdof@gmail.com>
2023-12-09 07:01:50 +0900

    #55 feat : 그림 생성 or Nft 생성 구현 완료

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/market/create/CreateNftOrPaintViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.aoztg.greengrim.presentation.ui.market.create
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class CreateNftOrPaintViewModel @Inject constructor(): ViewModel() {
-}
