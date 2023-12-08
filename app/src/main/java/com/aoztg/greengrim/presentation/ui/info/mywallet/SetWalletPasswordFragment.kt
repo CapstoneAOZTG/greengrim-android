f193541b4ed2f4a1b94efc4034ee6459a98e45a2
박진성 <plashdof@gmail.com>
2023-12-09 03:42:36 +0900

    #55 feat : SetWalletPassword 구현

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/info/mywallet/SetWalletPasswordFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/info/mywallet/SetWalletPasswordFragment.kt
@@ -1,8 +1,114 @@
 package com.aoztg.greengrim.presentation.ui.info.mywallet
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentSetWalletPasswordBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
+import com.aoztg.greengrim.presentation.ui.main.MainViewModel
+import com.aoztg.greengrim.presentation.util.Constants.TAG
+import dagger.hilt.android.AndroidEntryPoint
 
-class SetWalletPasswordFragment: BaseFragment<FragmentSetWalletPasswordBinding>(R.layout.fragment_set_wallet_password) {
+@AndroidEntryPoint
+class SetWalletPasswordFragment :
+    BaseFragment<FragmentSetWalletPasswordBinding>(R.layout.fragment_set_wallet_password) {
+
+    private val viewModel: SetWalletPasswordViewModel by viewModels()
+    private val parentViewModel: MainViewModel by activityViewModels()
+    private lateinit var numButtons: List<AppCompatButton>
+    private lateinit var passwordViews: List<ImageView>
+
+    companion object {
+        const val ERASE = 0
+        const val TYPE = 1
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        parentViewModel.hideBNV()
+        binding.vm = viewModel
+        setViewList()
+        setBtnListener()
+        initEventObserver()
+    }
+
+    private fun setViewList() {
+        with(binding) {
+            numButtons = listOf(
+                btnZero,
+                btnOne,
+                btnTwo,
+                btnThree,
+                btnFour,
+                btnFive,
+                btnSix,
+                btnSeven,
+                btnEight,
+                btnNine
+            )
+
+            passwordViews = listOf(
+                ivPw1, ivPw2, ivPw3, ivPw4, ivPw5, ivPw6
+            )
+        }
+    }
+
+    private fun setBtnListener() {
+        numButtons.forEachIndexed { i, btn ->
+            btn.setOnClickListener {
+                viewModel.typePassword(i.toString())
+            }
+        }
+
+        binding.btnErase.setOnClickListener {
+            viewModel.erasePassword()
+        }
+    }
+
+    private fun initEventObserver() {
+        repeatOnStarted {
+            viewModel.events.collect {
+                when (it) {
+                    is SetWalletPasswordEvents.TypePassword -> {
+                        setPasswordViews(TYPE, it.idx)
+                    }
+                    is SetWalletPasswordEvents.ErasePassword -> {
+                        setPasswordViews(ERASE, it.idx)
+                    }
+                    is SetWalletPasswordEvents.ChangeModeToCheck -> {
+                        binding.tvPasswordHeader.text = "지갑 비밀번호 확인"
+                        binding.tvPasswordAnnounce.text = "비밀번호를 다시 한번 입력해주세요"
+                        passwordViews.forEach { iv ->
+                            iv.setImageResource(R.drawable.icon_password_unfill)
+                        }
+                    }
+                    is SetWalletPasswordEvents.RemovePasswordViews -> {
+                        passwordViews.forEach { iv ->
+                            iv.setImageResource(R.drawable.icon_password_unfill)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setPasswordViews(type: Int, idx: Int) {
+        when (type) {
+            TYPE -> {
+                Log.d(TAG,idx.toString())
+                passwordViews[idx].setImageResource(R.drawable.icon_password_fill)
+            }
+
+            ERASE -> {
+                passwordViews[idx].setImageResource(R.drawable.icon_password_unfill)
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/info/mywallet/SetWalletPasswordViewModel.kt
@@ -0,0 +1,104 @@
+package com.aoztg.greengrim.presentation.ui.info.mywallet
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aoztg.greengrim.presentation.ui.BaseUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+sealed class SetWalletPasswordEvents {
+    data class TypePassword(val idx: Int) : SetWalletPasswordEvents()
+    data class ErasePassword(val idx: Int) : SetWalletPasswordEvents()
+    object ChangeModeToCheck : SetWalletPasswordEvents()
+    object RemovePasswordViews : SetWalletPasswordEvents()
+}
+
+data class SetWalletPasswordUiState(
+    val isCheckMode: Boolean = false,
+    val passwordCheckState: BaseUiState = BaseUiState.Empty
+)
+
+@HiltViewModel
+class SetWalletPasswordViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SetWalletPasswordUiState())
+    val uiState: StateFlow<SetWalletPasswordUiState> = _uiState.asStateFlow()
+
+    private val _events = MutableSharedFlow<SetWalletPasswordEvents>()
+    val events: SharedFlow<SetWalletPasswordEvents> = _events.asSharedFlow()
+
+    private val _curPassword = MutableStateFlow("")
+    val curPassword: StateFlow<String> = _curPassword.asStateFlow()
+
+    private var originPassword = ""
+
+    fun typePassword(text: String) {
+        if (curPassword.value.length < 5) {
+            _curPassword.value = curPassword.value + text
+            viewModelScope.launch {
+                _events.emit(SetWalletPasswordEvents.TypePassword(curPassword.value.length - 1))
+            }
+        } else if (curPassword.value.length == 5) {
+            _curPassword.value = curPassword.value + text
+            if (!uiState.value.isCheckMode) {
+                changeModeToCheck()
+            }
+        }
+    }
+
+    fun erasePassword() {
+        if (curPassword.value.isNotEmpty()) {
+            _curPassword.value = curPassword.value.substring(0, curPassword.value.length - 1)
+            viewModelScope.launch {
+                _events.emit(SetWalletPasswordEvents.ErasePassword(curPassword.value.length))
+            }
+        }
+    }
+
+    private fun changeModeToCheck() {
+        _uiState.update { state ->
+            state.copy(
+                isCheckMode = true
+            )
+        }
+        _curPassword.value = ""
+
+        originPassword = curPassword.value
+        observePasswordCheck()
+        viewModelScope.launch {
+            _events.emit(SetWalletPasswordEvents.ChangeModeToCheck)
+        }
+    }
+
+    private fun observePasswordCheck() {
+        _curPassword.onEach {
+            if (it.length == 6) {
+                if (it == originPassword) {
+                    // todo 지갑생성 post
+
+                } else {
+                    _curPassword.value = ""
+                    _uiState.update { state ->
+                        state.copy(
+                            passwordCheckState = BaseUiState.Error("비밀번호와 일치하지 않습니다")
+                        )
+                    }
+                    _events.emit(SetWalletPasswordEvents.RemovePasswordViews)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
