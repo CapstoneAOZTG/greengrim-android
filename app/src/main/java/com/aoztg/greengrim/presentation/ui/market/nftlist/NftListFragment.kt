8f4b929100ff759f5ba5b0b895601a5c6224260d
박진성 <plashdof@gmail.com>
2023-12-09 00:37:18 +0900

    #55 feat : nft list Fragment 생성

---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/market/nftlist/NftListFragment.kt
@@ -0,0 +1,8 @@
+package com.aoztg.greengrim.presentation.ui.market.nftlist
+
+import com.aoztg.greengrim.R
+import com.aoztg.greengrim.databinding.FragmentNftListBinding
+import com.aoztg.greengrim.presentation.base.BaseFragment
+
+class NftListFragment: BaseFragment<FragmentNftListBinding>(R.layout.fragment_nft_list) {
+}
