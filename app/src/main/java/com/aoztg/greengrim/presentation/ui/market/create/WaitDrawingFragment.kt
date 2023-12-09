adc118f515d611bf64d0518a3ae363ab1186c31d
박진성 <plashdof@gmail.com>
2023-12-09 12:23:29 +0900

    #55 feat : waiting drawing Fragment 레이아웃 구현

---
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/market/create/WaitDrawingFragment.kt
@@ -0,0 +1,11 @@
+package com.aoztg.greengrim.presentation.ui.market.create
+
+
+import com.aoztg.greengrim.R
+import com.aoztg.greengrim.databinding.FragmentWaitDrawaingBinding
+import com.aoztg.greengrim.presentation.base.BaseFragment
+
+class WaitDrawingFragment: BaseFragment<FragmentWaitDrawaingBinding>(R.layout.fragment_wait_drawaing) {
+
+
+}
