5fcbe4af6a5e022c633ff191126fb7837e6d57d5
박진성 <plashdof@gmail.com>
2023-12-09 23:37:43 +0900

    #55 feat : nft detail, paint detail 레이아웃 구현

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/market/create/WaitDrawingFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/market/create/WaitDrawingFragment.kt
@@ -1,11 +1,30 @@
 package com.aoztg.greengrim.presentation.ui.market.create
 
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentWaitDrawaingBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
 
-class WaitDrawingFragment: BaseFragment<FragmentWaitDrawaingBinding>(R.layout.fragment_wait_drawaing) {
+class WaitDrawingFragment :
+    BaseFragment<FragmentWaitDrawaingBinding>(R.layout.fragment_wait_drawaing) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnExit.setOnClickListener {
+            findNavController().toMarket()
+        }
+    }
+
+    private fun NavController.toMarket() {
+        val action = WaitDrawingFragmentDirections.actionWaitDrawingFragmentToMarketFragment()
+        val navOptions = NavOptions.Builder().setPopUpTo(R.id.main_nav, true).build()
+        navigate(action, navOptions)
+    }
 
 }
