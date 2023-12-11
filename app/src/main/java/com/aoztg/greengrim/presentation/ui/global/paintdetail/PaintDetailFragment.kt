848c7ad9cc4cc5b4ad9c9d0c27d877d56c2b52a6
박진성 <plashdof@gmail.com>
2023-12-12 00:25:36 +0900

    #55 feat : 인증상세 레이아웃 수정

--- app/src/main/java/com/aoztg/greengrim/presentation/ui/global/paintdetail/PaintDetailFragment.kt
+++ app/src/main/java/com/aoztg/greengrim/presentation/ui/global/paintdetail/PaintDetailFragment.kt
@@ -2,24 +2,28 @@ package com.aoztg.greengrim.presentation.ui.global.paintdetail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.aoztg.greengrim.R
 import com.aoztg.greengrim.databinding.FragmentPaintDetailBinding
 import com.aoztg.greengrim.presentation.base.BaseFragment
+import com.aoztg.greengrim.presentation.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PaintDetailFragment: BaseFragment<FragmentPaintDetailBinding>(R.layout.fragment_paint_detail) {
 
     private val viewModel: PaintDetailViewModel by viewModels()
+    private val parentViewModel: MainViewModel by activityViewModels()
     private val args: PaintDetailFragmentArgs by navArgs()
     private val grimId by lazy { args.grimId }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        parentViewModel.hideBNV()
         binding.vm = viewModel
         initEventObserver()
     }
