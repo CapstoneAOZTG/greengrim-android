3171fdab813c654e3d22df09385ac43620b74a29
박진성 <plashdof@gmail.com>
2024-05-12 15:01:26 +0900

    #88 version : 1.0.12 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.aoztg.greengrim"
         minSdk = 27
         targetSdk = 34
-        versionCode = 24
-        versionName = "1.0.11"
+        versionCode = 25
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "NAVER_CLIENT_ID", getProperty("naverClientId"))
